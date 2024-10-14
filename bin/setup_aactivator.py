34656acc10b8ce28e71567cebc875b99c509cb27
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2024-10-14 14:31:58 +0100

    Add an option to `setup-aactivator` to exclude files from git

--- bin/setup_aactivator.py
+++ bin/setup_aactivator.py
@@ -14,6 +14,11 @@ def main() -> int:
         nargs='?', default='venv',
         help='path to venv (default: %(default)s)',
     )
+    parser.add_argument(
+        '--git-exclude',
+        action='store_true',
+        help='add the aactivator scripts to the local git exclude file',
+    )
     args = parser.parse_args()
 
     activate_script = os.path.join(args.venv, 'bin', 'activate')
@@ -23,6 +28,11 @@ def main() -> int:
     with open('.deactivate.sh', 'w') as fd:
         fd.write('deactivate\n')
     os.chmod('.deactivate.sh', 0o600)
+
+    if args.git_exclude:
+        with open('.git/info/exclude', 'a') as fd:
+            fd.write('.activate.sh\n')
+            fd.write('.deactivate.sh\n')
 
     return 0
 
